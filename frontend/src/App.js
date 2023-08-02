1eb3d275cfe39022a44c03978666bc38741c88f1
Shubham <shubhamsingla0809@gmail.com>
2023-08-02 20:20:10 +1000

    Logo and Footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,19 @@
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+
 const App = () =>{
-  return <h1>Hello</h1>
+  return (
+    <>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  
+  );
 }
 export default App;
