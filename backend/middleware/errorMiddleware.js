8e6b2b3c3f4320580524392e6467fbd699fb38c8
Shubham <shubhamsingla0809@gmail.com>
2023-08-19 12:57:19 +1000

    Product routes and custom error handler

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,24 @@
+const notFound = (req, res, next) => {
+  const Error = new Error(`Not Found - ${req.originalUrl}`);
+  res.error(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    message = "Resource not found";
+    statusCode = 404;
+  }
+
+  res
+    .status(statusCode)
+    .json({
+      message,
+      stack: process.env.NODE_ENV === "production" ? "p" : err.stack,
+    });
+};
+
+export { notFound, errorHandler };
